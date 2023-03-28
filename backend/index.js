e189bd34dadf1ac24df2804fd7773ebe9b8b1306
Sreeram A <sriramlibra0@gmail.com>
2023-03-28 09:17:16 +0530

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,27 @@
 //create a express app
-const app=require('express')();
+const app = require("express")();
 
 //create a http server connecting our express app;
-const server=require('http').createServer(app);
+const server = require("http").createServer(app);
 
 //create a io server connectiong out http server
-const io=require('socket.io')(server, {
-  cors:{
-    origin:"http://localhost:3000"
-  }
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
 });
+
 
 io.on("connection", (socket) => {
-  console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-});
-
-io.on("connection" , (socket)=>{
-  console.log("socket" , socket);
+  // console.log("socket", socket);
   console.log("socket is connected");
 
-  socket.on("chat" , (payload)=>{
-    console.log("What is payload:" ,payload);
-    io.emit("chat" , payload);
-  })
+  socket.on("chat", (payload) => {
+    console.log("What is payload:", payload);
+    io.emit("chat", payload);
+  });
+});
 
-})
-// io.listen(8000)
-
-server.listen(5000,()=>{console.log("app running on port 5000");});
+server.listen(5000, () => {
+  console.log("app running on port 5000");
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,29 @@ import io, { Socket } from "socket.io-client";
 
 const socket = io.connect("http://localhost:5000");
 
+const user=nanoid(5);
+
 function App() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
 
   const sendChat=(e)=>{
     e.preventDefault();
-    socket.emit("chat" , message);
+    socket.emit("chat" , {message:message , user:user});
     setMessage("");
   }
-
+useEffect(()=>{
+  socket.on('chat' , (payload)=>{
+    setChat([...chat , payload])
+  })
+})
 
   return (
     <div className="App">
+
+      {chat.map((payload , index)=>{
+        return <p key={index}> {payload.message} : <span>id: {payload.user}</span> </p>
+      })}
       <form onSubmit={sendChat}>
         <input
           type="text"
